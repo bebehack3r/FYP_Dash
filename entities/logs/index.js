e1e59419ccc3dcfd2ce609e99520d119535f7817
bebehack3r <20886900+bebehack3r@users.noreply.github.com>
2024-04-06 14:37:36 +0800

    ip location resolve is moved to backend

--- entities/logs/index.js
+++ entities/logs/index.js
@@ -1,21 +1,49 @@
 import fs from 'fs';
 import path from 'path';
 import readline from 'readline';
+import ipLocation from 'iplocation';
 
-const analyzeLogEntry = (logEntry, lineNum, analysis_res) => {
+const filterIP = (line) => {
+  const ipReg = /\} (.*) \-\>/;
+  const ipAddr = line.match(ipReg);
+  return ipAddr[1];
+};
+
+const fetchIPLocation = async (source) => {
+  const ip = source.split(':')[0];
+  const split = ip.split('.');
+  if(split[0] == '10' || split[0] == '100') return null;
+  if((split[0] == '172' && (+split[1] >= 16 && +split[1] <= 31)) || (split[0] == '192' && split[1] == '168')) return null;
+  try {
+    const location = await ipLocation(source.split(':')[0]);
+    if(location.latitude) return [location.latitude, location.longitude].join(',');
+    return null;
+  } catch(err) {
+    return null;
+  }
+};
+
+const analyzeLogEntry = async (logEntry, lineNum, analysis_res) => {
   const defaultChecks = [
     { entry: 'ET SCAN', type: 'scan' },
     { entry: 'ET POLICY', type: 'policy' },
     { entry: 'ET INFO', type: 'sus' }
   ];
-  const spots = defaultChecks.map(check => {
-    if(logEntry.includes(check.entry)) return { 
-      type: check.type, 
-      line: logEntry, 
-      number: lineNum 
-    };
-    return null;
-  });
+  const spots = [];
+  for(let i = 0; i < defaultChecks.length; i++) {
+    if(logEntry.includes(defaultChecks[i].entry)) {
+      console.log('HUH?');
+      const ip = filterIP(logEntry);
+      console.log(ip);
+      spots.push({ 
+        threatType: defaultChecks[i].type, 
+        sourceLine: logEntry, 
+        lineNumber: lineNum,
+        ipAddress: ip,
+        ipLocation: await fetchIPLocation(ip)
+      });
+    }
+  };
   spots.map(spot => { if(spot) analysis_res.push(spot) });
 };
 
@@ -77,7 +105,7 @@ export const analyze = (req, res) => {
     query = 'SELECT * FROM autodetected WHERE logID = ?';
     req.databaseConnection.all(query, [id], (err, rows) => {
       if (err) return res.status(500).json({ message: 'ERROR', data: err.message });
-      if (!rows) {
+      if (!rows.length) {
         const p = path.join(path.resolve(), `uploads/${row.fname}`);
         const analysis_res = [];
         const readInterface = readline.createInterface({
@@ -93,8 +121,8 @@ export const analyze = (req, res) => {
         readInterface.on('close', () => {
           req.databaseConnection.serialize(() => {
             req.databaseConnection.run('BEGIN TRANSACTION');
-            const stmt = req.databaseConnection.prepare('INSERT INTO autodetected (logID, threatType, sourceLine, lineNumber) VALUES (?, ?, ?, ?)');
-            analysis_res.forEach(obj => stmt.run(id, obj.type, obj.line, obj.number));
+            const stmt = req.databaseConnection.prepare('INSERT INTO autodetected (logID, threatType, sourceLine, lineNumber, ipAddress, ipLocation) VALUES (?, ?, ?, ?, ?, ?)');
+            analysis_res.forEach(obj => stmt.run(id, obj.threatType, obj.sourceLine, obj.lineNumber, obj.ipAddress, obj.ipLocation));
             req.databaseConnection.run('COMMIT', (err) => {
               if (err) return res.status(500).json({ message: 'ERROR', data: err.message });
             });
